b8c8b8b80199e81cab565bd453009a3c6d11870d
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2020-06-17 23:07:13 -0300

    Criando GlobalStyles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+import GlobalStyle from "./styles/GlobalStyles";
+
 function App() {
-  return <div>Hi</div>;
+  return (
+    <>
+      <div>Hello</div>
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;
