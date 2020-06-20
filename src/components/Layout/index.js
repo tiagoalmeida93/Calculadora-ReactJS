9556015ae2df573f7690798a2e3b56a28f265e2d
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2020-06-20 16:24:29 -0300

    Finalizando ServerName

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 import { Grid } from "./styles";
+
 import ServerList from "../ServerList";
+import ServerName from "../ServerName";
 
 export default function Layout() {
   return (
     <Grid>
       <ServerList />
+      <ServerName />
     </Grid>
   );
 }
---
+++ src/components/ServerName/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Container, Title, ExpandIcon } from "./styles";
+
+export default function ServerName() {
+  return (
+    <Container>
+      <Title>Servidor do @TiagoAlmeidz</Title>
+      <ExpandIcon />
+    </Container>
+  );
+}
