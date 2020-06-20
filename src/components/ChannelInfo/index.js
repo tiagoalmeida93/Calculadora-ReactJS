318c60c1a3251ea27c5d269e2fb3d2a7e29487af
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2020-06-20 17:01:26 -0300

    Finalizando component ChannelInfo

---
+++ src/components/ChannelInfo/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import {
+  Container,
+  HashtagIcon,
+  Title,
+  Separator,
+  Description,
+} from "./styles";
+
+export default function ChannelInfo() {
+  return (
+    <Container>
+      <HashtagIcon />
+
+      <Title>chat-livre</Title>
+      <Separator />
+      <Description>Canal aberto para conversas</Description>
+    </Container>
+  );
+}
