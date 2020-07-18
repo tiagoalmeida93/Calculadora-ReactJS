b43bf3738a54ad5de947a3cd3bef74540422ecce
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2020-07-18 14:12:57 -0300

    Finalizando component UserList

--- src/components/UserList/index.js
+++ src/components/UserList/index.js
@@ -5,7 +5,7 @@ import { Container, Role, User, Avatar } from "./styles";
 const UserRow = ({ nickname, isBot }) => {
   return (
     <User>
-      <Avatar />
+      <Avatar className={isBot ? "bot" : ""} />
 
       <strong>{nickname}</strong>
 
@@ -19,9 +19,47 @@ export default function UserList() {
     <Container>
       <Role>Disponível - 1</Role>
       <UserRow nickname="Tiago Almeida" />
-
       <Role>Offline - 19</Role>
+      <UserRow nickname="Android" />
+      <UserRow nickname="Iron Main" />
+      <UserRow nickname="Mario" />
+      <UserRow nickname="Stiff" />
+      <UserRow nickname="Yosh" />
       <UserRow nickname="Kratos" isBot />
+      <UserRow nickname="unknown" />
+      <UserRow nickname="unknown" />
+      <UserRow nickname="unknown" />
+      <UserRow nickname="unknown" />
+      <UserRow nickname="unknown" />
+      <UserRow nickname="unknown" />
+      <UserRow nickname="unknown" />
+      <UserRow nickname="unknown" />
+      <UserRow nickname="unknown" />
+      <UserRow nickname="unknown" />
+      <UserRow nickname="unknown" />
+      <UserRow nickname="unknown" isBot />
+      <UserRow nickname="unknown" />
+      <UserRow nickname="unknown" isBot />
+      <UserRow nickname="unknown" />
+      <UserRow nickname="unknown" />
+      <UserRow nickname="unknown" />
+      <UserRow nickname="unknown" />
+      <UserRow nickname="unknown" />
+      <UserRow nickname="unknown" isBot />
+      <UserRow nickname="unknown" isBot />
+      <UserRow nickname="unknown" isBot />
+      <UserRow nickname="unknown" isBot />
+      <UserRow nickname="unknown" isBot />
+      <UserRow nickname="unknown" />
+      <UserRow nickname="unknown" />
+      <UserRow nickname="unknown" />
+      <UserRow nickname="unknown" />
+      <UserRow nickname="unknown" isBot />
+      <UserRow nickname="unknown" isBot />
+      <UserRow nickname="unknown" isBot />
+      <UserRow nickname="unknown" isBot />
+      <UserRow nickname="unknown" />
+      <UserRow nickname="unknown" isBot />
     </Container>
   );
 }
--- src/components/UserList/styles.js
+++ src/components/UserList/styles.js
@@ -1,19 +1,91 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+  grid-area: UL;
 
-  padding: 0 11px 0 16px;
+  display: flex;
+  flex-direction: column;
+
+  padding: 3px 6px 0 16px;
   background-color: var(--secondary);
 
-  box-shadow: rgba(0, 0, 0, 0.2) 0px 1px 0px 0px;
-  z-index: 2;
+  max-height: calc(100vh - 46px);
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    width: 5px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: var(--tertiary);
+    border-radius: 4px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: var(--secondary);
+  }
 `;
 
-export const Role = styled.div``;
+export const Role = styled.span`
+  margin-top: 20px;
 
-export const User = styled.div``;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 500;
+  color: var(--gray);
+`;
 
-export const Avatar = styled.div``;
+export const User = styled.div`
+  margin-top: 5px;
+  padding: 5px;
+
+  display: flex;
+  align-items: center;
+
+  cursor: pointer;
+
+  border-radius: 4px;
+  background: transparent;
+  transition: background 0.2s;
+  &:hover {
+    background: rgba(255, 255, 255, 0.1);
+  }
+
+  > strong {
+    margin-left: 13px;
+    font-weight: 500;
+    color: var(--white);
+    opacity: 0.7;
+
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
+
+  > span {
+    margin-left: 9px;
+
+    background-color: var(--discord);
+    color: var(--white);
+    border-radius: 4px;
+    padding: 4px 5px;
+
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 11px;
+  }
+`;
+
+export const Avatar = styled.div`
+  flex-shrink: 0;
+
+  width: 32px;
+  height: 32px;
+
+  border-radius: 50%;
+  background-color: var(--primary);
+
+  &.bot {
+    background-color: var(--mention-detail);
+  }
+`;
